d411a8a4fa6750b1ac4d70762d810638f2c08d48
navya.athiraram@databricks.com <navya.athiraram@databricks.com>
2023-02-27 07:28:50 +0000

    Merge conflict commit

--- abc.py
+++ abc.py
@@ -3,3 +3,15 @@ import requests
 wid = input("Enter workspace URL: ")
 data = requests.get(wid)
 print (data)
+
+# COMMAND ----------
+
+# MAGIC %sh traceroute adb-624783076386327.7.azuredatabricks.net
+
+# COMMAND ----------
+
+# MAGIC %sql select current_metastore()
+
+# COMMAND ----------
+
+dbutils.fs.ls("dbfs:/FileStore")
