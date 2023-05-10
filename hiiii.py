d8bc3ee2284c6582797f8e53458b49ef1ef8835e
navyaathiraram <85556564+navyaathiraram@users.noreply.github.com>
2023-05-10 08:37:22 +0530

    Update hiiii.py

--- hiiii.py
+++ hiiii.py
@@ -1,10 +1,1 @@
-# Databricks notebook source
-this is from databricks
-
-# COMMAND ----------
-
-this is from databricks after confirming git tokens
-
-# COMMAND ----------
-
-this is with limited perms token
+import pip
